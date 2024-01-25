6405fe83b27fc1a9c6171012a624fb78798943e7
GoyalSidhant <sidhant702@gmail.com>
2024-01-26 02:46:24 +0530

    function views + serializer.Serializer

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render
-from watchlist_app.models import Movie
-from django.http import JsonResponse
-# Create your views here.
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+# from django.http import JsonResponse
+# # Create your views here.
 
 
-def movie_list(request):
-    movies = Movie.objects.all()
-    print(movies.values())
-    return JsonResponse({'data': list(movies.values())})
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     print(movies.values())
+#     return JsonResponse({'data': list(movies.values())})
 
 
-def movie_details(request,  pk):
-    movie = Movie.objects.get(pk=pk)
-    return JsonResponse({movie}) ## error 
+# def movie_details(request,  pk):
+#     movie = Movie.objects.get(pk=pk)
+#     return JsonResponse({movie}) ## error 
 
